36ba0bcb133d6a897b3c6ac860bc34b4f0b6075e
ShrutiVerma17 <connect.shrutiv@gmail.com>
2019-12-08 16:11:55 -0500

    Final SpecialProgramsBar

---
+++ src/components/HelpPageTitle.jsx
@@ -0,0 +1,214 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    height: 25px;
+    width: 100%;
+    position: relative;
+    top: 460px;
+    left: 150px;
+    font-size: 27px;
+    font-weight: 900;
+    font-family: "HelveticaNeue-CondensedBold", "Helvetica Neue";
+    color: #707070;
+    text-align: left;
+`;
+
+const Wrapper = styled.div`
+    background-color: white;
+`;
+const Line = styled.div`
+    position: relative;
+    top: 475px;
+`;
+const TinyContainer = styled.div`
+    height: 135px;
+    background-color: #F4F4F4;
+    opacity: 0.7;
+    width: 250px;
+    position: relative;
+    top: 10px;
+    margin-left: 150px;
+    border-radius: 15px;
+    text-align: center;
+    font-size: 15px;
+    font-family: "Helvetica Neue";
+`;
+const ContainerTitle = styled.div`
+    margin-top: 500px;
+    margin-left: 250px;
+    font-size: 17px;
+    font-family: "Helvetica Neue";
+    color: #707070;
+    font-weight: bold;
+`;
+
+const TinyContainer2 = styled.div`
+    height: 135px;
+    background-color: #F4F4F4;
+    opacity: 0.7;
+    width: 250px;
+    position: relative;
+    top: 10px;
+    margin-left: 450px;
+    border-radius: 15px;
+    text-align: center;
+    font-size: 15px;
+    font-family: "Helvetica Neue";
+`;
+const ContainerTitle2 = styled.div`
+    margin-top: -155px;
+    margin-left: 480px;
+    font-size: 17px;
+    font-family: "Helvetica Neue";
+    color: #707070;
+    font-weight: bold;
+`;
+
+const TinyContainer3 = styled.div`
+    height: 135px;
+    background-color: #F4F4F4;
+    opacity: 0.7;
+    width: 250px;
+    position: relative;
+    top: 10px;
+    margin-left: 750px;
+    border-radius: 15px;
+    text-align: center;
+    font-size: 15px;
+    font-family: "Helvetica Neue";
+`;
+const ContainerTitle3 = styled.div`
+    margin-top: -155px;
+    margin-left: 780px;
+    font-size: 17px;
+    font-family: "Helvetica Neue";
+    color: #707070;
+    font-weight: bold;
+`;
+
+const TinyContainer4 = styled.div`
+    height: 135px;
+    background-color: #F4F4F4;
+    opacity: 0.7;
+    width: 250px;
+    position: relative;
+    top: 10px;
+    margin-left: 1050px;
+    border-radius: 15px;
+    text-align: center;
+    font-size: 15px;
+    font-family: "Helvetica Neue";
+`;
+const ContainerTitle4 = styled.div`
+    margin-top: -156px;
+    margin-left: 1090px;
+    font-size: 17px;
+    font-family: "Helvetica Neue";
+    color: #707070;
+    font-weight: bold;
+`;
+
+const TinyContainer5 = styled.div`
+    height: 135px;
+    background-color: #F4F4F4;
+    opacity: 0.7;
+    width: 250px;
+    position: relative;
+    top: 10px;
+    margin-left: 150px;
+    border-radius: 15px;
+    text-align: center;
+    font-size: 15px;
+    font-family: "Helvetica Neue";
+`;
+const ContainerTitle5 = styled.div`
+    margin-top: 50px;
+    margin-left: 177px;
+    font-size: 17px;
+    font-family: "Helvetica Neue";
+    color: #707070;
+    font-weight: bold;
+`;
+
+const TinyContainer6 = styled.div`
+    height: 135px;
+    background-color: #F4F4F4;
+    opacity: 0.7;
+    width: 250px;
+    position: relative;
+    top: 10px;
+    margin-left: 450px;
+    border-radius: 15px;
+    text-align: center;
+    font-size: 15px;
+    font-family: "Helvetica Neue";
+`;
+const ContainerTitle6 = styled.div`
+    margin-top: -155px;
+    margin-left: 500px;
+    font-size: 17px;
+    font-family: "Helvetica Neue";
+    color: #707070;
+    font-weight: bold;
+`;
+
+const TinyContainer7 = styled.div`
+    height: 135px;
+    background-color: #F4F4F4;
+    opacity: 0.7;
+    width: 250px;
+    position: relative;
+    top: 10px;
+    margin-left: 750px;
+    border-radius: 15px;
+    text-align: center;
+    font-size: 15px;
+    font-family: "Helvetica Neue";
+`;
+const ContainerTitle7 = styled.div`
+    margin-top: -155px;
+    margin-left: 770px;
+    font-size: 17px;
+    font-family: "Helvetica Neue";
+    color: #707070;
+    font-weight: bold;
+`;
+
+const TinyContainer8 = styled.div`
+    height: 135px;
+    background-color: #F4F4F4;
+    opacity: 0.7;
+    width: 250px;
+    position: relative;
+    top: 10px;
+    margin-left: 1050px;
+    border-radius: 15px;
+    text-align: center;
+    font-size: 15px;
+    font-family: "Helvetica Neue";
+`;
+const ContainerTitle8 = styled.div`
+    margin-top: -155px;
+    margin-left: 1110px;
+    font-size: 17px;
+    font-family: "Helvetica Neue";
+    color: #707070;
+    font-weight: bold;
+`;
+class SpecialProgramBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <Wrapper>
+        <h>MAJOR HELP</h>
+        </Wrapper>
+    );
+  }
+}
+
+SpecialProgramBar.label = "SpecialProgramBar";
+export default SpecialProgramBar;
